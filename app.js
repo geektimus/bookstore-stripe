3ca8541bfda4eeecd293e6ce1e01298a58f73b33
Geektimus <alexander.kno@gmail.com>
2017-10-10 17:39:19 -0500

    Add basic routes and handlebars base templates

--- app.js
+++ app.js
@@ -5,6 +5,22 @@ const exphbs = require("express-handlebars");
 
 const app = express();
 
+// Handlebars middleware
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+// Body parser
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// Set static folder
+app.use(express.static(`${__dirname}/public`));
+
+// Index Route
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
