bdf1c6599255d96884b00dba0e1cb94c622e3494
Geektimus <alexander.kno@gmail.com>
2017-10-10 18:27:53 -0500

    Add charge route for payment, Add success route

--- app.js
+++ app.js
@@ -21,6 +21,28 @@ app.get("/", (req, res) => {
   res.render("index");
 });
 
+// Charge Route
+app.post("/charge", (req, res) => {
+  const amount = 2500;
+  stripe.customers
+    .create({
+      email: req.body.stripeEmail,
+      source: req.body.stripeToken
+    })
+    .then(customer => {
+      stripe.charges
+        .create({
+          amount,
+          description: "High Performance Spark Ebook",
+          currency: "usd",
+          customer: customer.id
+        })
+        .then(charge => {
+          res.render("success");
+        });
+    });
+});
+
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
